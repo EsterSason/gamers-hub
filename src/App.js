f0b7d65070f0537b370e8561d8a7337cff44c35f
unknown <est123123@gmail.com>
2018-10-14 10:51:04 +0300

    unfinished work but needs backup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Register from './register/Register';
 import Header from './header/Header';
+import Login from './login/Login';
 
 const App = () => (
-  <div>
-    <Header />
-    <Register />
-  </div>
+  <Router>
+    <div>
+      <Header />
+      {/* <Route path="/" component={App} /> */}
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </div>
+  </Router>
 );
 
 // class App extends Component {
